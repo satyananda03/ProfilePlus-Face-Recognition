6f4a817387cb75acc34a524ac6caeefa2cc150f2
Satyananda <gedesatyananda@gmail.com>
2026-01-13 23:02:16 +0700

    init project

---
+++ src/routes.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+# Gunakan APIRouter, bukan FastAPI
+router = APIRouter()
+
+current_file_dir = Path(__file__).resolve().parent
+templates_path = current_file_dir / "templates"
+templates = Jinja2Templates(directory=templates_path)
+
+@router.get("/detect", response_class=HTMLResponse)
+async def face_validation_page(request: Request):
+    return templates.TemplateResponse("face-detection.html", {
+        "request": request
+    })
